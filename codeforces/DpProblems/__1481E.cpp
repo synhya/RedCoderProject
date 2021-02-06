5fb73800d8cbafd4c58fdd9a0e4e40ffa21122e4
AE <wkddnjsdlr1@gmail.com>
2021-02-07 01:44:30 +0900

    Codeforces Round #699 review

---
+++ codeforces/DpProblems/__1481E.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(c) c.begin(), c.end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep2(i, a, b) for (int i = a; i <= b; ++i)
+#define rep3(i, a, b) for (int i = a; i >= b; --i)
+#define endl '\n'
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+const string YESNO[2] = {"NO", "YES"};
+const string YesNo[2] = {"No", "Yes"};
+const string yesno[2] = {"no", "yes"};
+void YES(bool t = 1) { cout << YESNO[t] << endl; }
+void Yes(bool t = 1) { cout << YesNo[t] << endl; }
+void yes(bool t = 1) { cout << yesno[t] << endl; }
+
+template <class T, class S>
+inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
+template <class T, class S>
+inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
+#define INT(...)     \
+    int __VA_ARGS__; \
+    IN(__VA_ARGS__)
+#define LL(...)     \
+    ll __VA_ARGS__; \
+    IN(__VA_ARGS__)
+#define VEC(type, name, size) \
+    vector<type> name(size);  \
+    IN(name)
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define VV(type, name, h, w)                       \
+    vector<vector<type>> name(h, vector<type>(w)); \
+    IN(name)
+int scan() { return getchar(); }
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S>
+void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T>
+void scan(vector<T> &);
+template <class T>
+void scan(vector<T> &a) {
+    for (auto &i : a)
+        scan(i);
+}
+template <class T>
+void scan(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail>
+void IN(Head &head, Tail &...tail) {
+    scan(head);
+    IN(tail...);
+}
+template <class T>
+vector<T> divisor(T x) {
+    vector<T> ans;
+    for (T i = 1; i * i <= x; i++)
+        if (x % i == 0) {
+            ans.pb(i);
+            if (i * i != x)
+                ans.pb(x / i);
+        }
+    return ans;
+}
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+// constexpr int inf = 1e9;
+// constexpr i64 inf = 1e18;
+// const int N = 500 * 1000 + 5; // use for N <= 5 * 10^5
+// const int MX = 1e9 + 7; // For convenience, find the answer modulo 10^9+7
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    INT(n);
+    vector<int> a(n), f(n), cf(n);
+    vector<pair<int, int>> at(n, {-1, -1});
+    rep(i, n) {
+        cin >> a[i];
+        a[i]--;
+        f[a[i]]++; // how many between at pair.
+        if (at[a[i]].first == -1)
+            at[a[i]].first = i;
+        at[a[i]].second = i;
+    }
+    vector<int> dp(n + 1);
+    rep3(i, n - 1, 0) { // find the books that will NOT MOVE!!
+        dp[i] = dp[i + 1];
+        // 이번책을 그냥 움직이는 경우.(minimum initiate)
+        cf[a[i]]++;
+        if (i == at[a[i]].first)
+            dp[i] = max(dp[i], dp[at[a[i]].second + 1] + f[a[i]]);
+        //현재색깔 마지막이므로. 범위내 현재색깔은 안움직임
+        // + 범위바깥의 dp가져오기 (dp[at[a[i]].second + 1])
+        else
+            dp[i] = max(dp[i], cf[a[i]]);
+        //현재 색깔 책 빼고 다 움직인경우.. 현재색 책만 안움직임
+    }
+    cout << n - dp[0] << endl; // maximum not move == minimum move
+
+    return 0;
+}
